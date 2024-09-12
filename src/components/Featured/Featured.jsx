f7081e425301f3a4e308cc06dad0a139c5de5514
kirk-en <kirkenbysk@gmail.com>
2024-09-12 12:43:45 -0400

    finalize logo scrolling carousel

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -3,13 +3,18 @@ import "./Featured.scss";
 const logoFiles = [
   "ABC-News-Logo-2007.png",
   "Columbia-Business-School-Logo-FC-2.png",
+  "forbes-logo-black-transparent.png",
   "Fox_News_Logo.png",
   "fox-29-philadelphia-logo.png",
   "general-electric-logo-png-transparent.png",
   "GMA_logo_2021.png",
+  "GQ-logo.svg",
   "HBS-styleguide-primary-logo-3-1024x507.png",
+  "Johns-Hopkins-University-Logo.png",
+  "Northwestern_University_seal.png",
   "nyt.png",
   "QVC-Logo.png",
+  "TED-Logo.png",
   "time-logo-transparent.png",
   "tlc-logo-logo.png",
   "Today_logo.svg.png",
@@ -18,6 +23,8 @@ const logoFiles = [
 ];
 
 const Featured = () => {
+  const doubleLogos = [...logoFiles, ...logoFiles];
+
   return (
     <section className="featured">
       <h2 className="featured__title">Featured:</h2>
@@ -28,7 +35,18 @@ const Featured = () => {
               src={`${import.meta.env.BASE_URL}logos/${logo}`}
               alt=""
               key={index}
-              height="100px"
+              className="featured__logo"
+            />
+          );
+        })}
+      </figure>
+      <figure className="featured__marquee">
+        {logoFiles.map((logo, index) => {
+          return (
+            <img
+              src={`${import.meta.env.BASE_URL}logos/${logo}`}
+              alt=""
+              key={index}
               className="featured__logo"
             />
           );
