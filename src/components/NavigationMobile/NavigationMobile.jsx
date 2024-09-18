726867519a0ea72d61ac7168b14eba0c48bccaca
kirk-en <kirkenbysk@gmail.com>
2024-09-18 12:09:49 -0400

    add mobile nav component, add styling to display standard vs mobile nav

---
+++ src/components/NavigationMobile/NavigationMobile.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const NavigationMobile = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <div>mobile nav</div>
+    </>
+  );
+};
+
+export default NavigationMobile;
