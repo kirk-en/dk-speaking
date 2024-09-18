26808689d3f05bc0f3016f13a5ddf2903ebbec40
kirk-en <kirkenbysk@gmail.com>
2024-09-18 15:23:14 -0400

    enable state and create styling for mobile menu

--- src/components/NavigationMobile/NavigationMobile.jsx
+++ src/components/NavigationMobile/NavigationMobile.jsx
@@ -1,6 +1,8 @@
 import { Menu } from "@mui/icons-material";
 import { useState } from "react";
 import "./NavigationMobile.scss";
+import { Drawer } from "@mui/material";
+import Navigation from "../Navigation/Navigation";
 
 const NavigationMobile = () => {
   const [open, setOpen] = useState(false);
@@ -10,9 +12,38 @@ const NavigationMobile = () => {
       <Menu
         className="mobile-nav__open"
         onClick={() => {
-          console.log("mobile menu button clicked!!!");
+          setOpen(true);
         }}
       />
+      <Drawer
+        open={open}
+        anchor="top"
+        className="mobile-nav"
+        onClose={() => setOpen(false)}
+      >
+        <ul className="mobile-nav__list">
+          <li className="mobile-nav__list-item">
+            <a href="#" className="mobile-nav__link">
+              about
+            </a>
+          </li>
+          <li className="mobile-nav__list-item">
+            <a href="#" className="mobile-nav__link">
+              speaking
+            </a>
+          </li>
+          <li className="mobile-nav__list-item">
+            <a href="#" className="mobile-nav__link">
+              coaching
+            </a>
+          </li>
+          <li className="mobile-nav__list-item">
+            <a href="#" className="mobile-nav__link">
+              contact
+            </a>
+          </li>
+        </ul>
+      </Drawer>
     </>
   );
 };
