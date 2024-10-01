0abc9e80087c8e92de239a1e2e2bf3214883709b
Kirk Enbysk <kirkenbysk@gmail.com>
2024-10-01 17:48:00 -0400

    modify quote section styles

--- src/components/DynamicQuote/DynamicQuote.jsx
+++ src/components/DynamicQuote/DynamicQuote.jsx
@@ -1,4 +1,5 @@
 import quoteData from "../../assets/testimonials.json";
+import "./DynamicQuote.scss";
 
 const DynamicQuote = () => {
   let quoteSeed = Math.floor(Math.random() * quoteData.length);
@@ -6,7 +7,7 @@ const DynamicQuote = () => {
 
   return (
     <div className="quote__text">
-      <h2>Project Your Presence</h2>
+      <h2>{quoteData[quoteSeed].headline}</h2>
       <h3>{`"${quoteData[quoteSeed].quote}"`}</h3>
       <p>- {quoteData[quoteSeed].name}</p>
       <p>{`${quoteData[quoteSeed].title}, ${quoteData[quoteSeed].company}`}</p>
