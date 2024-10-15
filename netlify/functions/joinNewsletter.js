1c179f26a3d4d4876293a7c3a1bccf0389f4e9bf
Kirk Enbysk <kirkenbysk@gmail.com>
2024-10-15 17:16:48 +0200

    convert joinNewsletter to serverless function

--- netlify/functions/joinNewsletter.js
+++ netlify/functions/joinNewsletter.js
@@ -1,22 +1,40 @@
-import { SibApiV3Sdk } from "sib-api-v3-sdk";
-let defaultClient = SibApiV3Sdk.ApiClient.instance;
+import SibApiV3Sdk from "sib-api-v3-sdk";
 
-let apiKey = defaultClient.authentications["api-key"];
-apiKey.apiKey = process.env.VITE_BREVO_API_KEY;
+export async function handler(event, context) {
+  try {
+    // Parse incoming data from form
+    const formObj = JSON.parse(event.body);
+    let contactName = formObj.name.split(" ");
 
-let apiInstance = new SibApiV3Sdk.ContactsApi();
+    // Brevo setup
+    let defaultClient = SibApiV3Sdk.ApiClient.instance;
+    let apiKey = defaultClient.authentications["api-key"];
+    apiKey.apiKey = process.env.VITE_BREVO_API_KEY;
+    let apiInstance = new SibApiV3Sdk.ContactsApi();
 
-let newContact = new SibApiV3Sdk.CreateContact();
+    // Create new contact for email list
+    let newContact = new SibApiV3Sdk.CreateContact();
+    newContact.email = formObj.email;
+    if (contactName.length >= 1) {
+      newContact.attributes = {
+        FIRSTNAME: contactName[0],
+        LASTNAME: contactName.length > 1 ? contactName[1] : "",
+      };
+    }
+    newContact.listIds = [5];
 
-newContact.EMAIL = "kirkenbysk@gmail.com";
-newContact.FIRSTNAME = "Kirk";
-newContact.LASTNAME = "Testbysk";
+    const data = await apiInstance.createContact(newContact);
 
-apiInstance.createContact(newContact).then(
-  function (data) {
     console.log("Sucessfully hit Brevo API. Data: " + JSON.stringify(data));
-  },
-  function (error) {
-    console.log(error);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Contact added successfully", data }),
+    };
+  } catch (error) {
+    console.error("Error occurred: ", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
   }
-);
+}
