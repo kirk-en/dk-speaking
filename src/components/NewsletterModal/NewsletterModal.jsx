55a63fd45840c74ab493be61974b755aa37d8706
Kirk Enbysk <kirkenbysk@gmail.com>
2024-10-18 18:49:07 +0200

    add fields + form state to modal and connect to newsletter backend, modal styling

--- src/components/NewsletterModal/NewsletterModal.jsx
+++ src/components/NewsletterModal/NewsletterModal.jsx
@@ -1,9 +1,52 @@
 import "./NewsletterModal.scss";
 import ilsLogo from "../../assets/ils-logo.png";
+import { TextField } from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
 
 const newsletterToken = localStorage.getItem("newsletterToken");
 
 const NewsletterModal = () => {
+  const [hasError, setHasError] = useState({
+    name: false,
+    email: false,
+    submit: false,
+  });
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    newsletter: true,
+    submit: false,
+  });
+
+  const joinNewsletter = async (formObj) => {
+    try {
+      await axios.post(`${import.meta.env.VITE_NEWSLETTER_URL}`, formObj, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (hasError.submit === true)
+        setHasError((prevValues) => ({ ...prevValues, submit: false }));
+      setFormValues((prevValues) => ({ ...prevValues, submit: true }));
+    } catch (error) {
+      console.log(error);
+      setHasError((prevValues) => ({ ...prevValues, submit: true }));
+    }
+  };
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((prevValues) => {
+      return { ...prevValues, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    joinNewsletter(formValues);
+  };
+
   return (
     <article className="modal">
       <div className="modal__content">
@@ -16,16 +59,52 @@ const NewsletterModal = () => {
           <h2 className="modal__main modal__main--title">
             Subscribe to the Ivy Level Speaking newsletter.
           </h2>
-          <p className="modal__main modal__terms">
-            Please verify your age to view Pines Play Studios.
-          </p>
+          {hasError.submit ? (
+            <p className="modal__main modal__terms modal__terms--red">
+              Our apologies! There was an error submitting, please try again
+              later.
+            </p>
+          ) : (
+            <p className="modal__main modal__terms">
+              Join now for expert tips, exclusive content, and the latest
+              updates to help you enhance your public speaking skills. 📫
+            </p>
+          )}
           <div className="modal__form-container">
-            <button className="modal__button modal__close">Submit</button>
+            <form onSubmit={handleSubmit}>
+              <div className="form__dual-field">
+                <TextField
+                  className="form__small-field form__field"
+                  label="Name"
+                  id="name"
+                  name="name"
+                  error={hasError.name}
+                  value={formValues.name}
+                  onChange={handleFormChange}
+                />
+                <TextField
+                  className="form__small-field form__field"
+                  label="Email"
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  aria-required="true"
+                  error={hasError.email}
+                  value={formValues.email}
+                  onChange={handleFormChange}
+                  helperText={hasError.email ? "Email is required." : ""}
+                />
+              </div>
+              <button className="modal__button modal__close" type="submit">
+                Submit
+              </button>
+            </form>
           </div>
-          <p className="modal__terms">
+          {/* <p className="modal__terms">
             By entering this website you are agreeing to our terms of use and
             privacy policy.
-          </p>
+          </p> */}
         </div>
       </div>
     </article>
